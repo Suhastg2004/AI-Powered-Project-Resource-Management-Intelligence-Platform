dacc3a84f65b330fa8f1d50c1589bb525ff4eaf3
saiyeshwin <saiyeshwin@gmail.com>
2026-05-30 20:24:34 +0530

    update[Yeshwin]- updated platform security configuration

--- platform/src/main/java/com/seveneleven/platform/config/SecurityConfig.java
+++ platform/src/main/java/com/seveneleven/platform/config/SecurityConfig.java
@@ -154,25 +154,16 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(
-                List.of("http://localhost:5173"));
-
+        configuration.setAllowedOriginPatterns(
+        List.of("*"));
         configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
+            List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(
-                List.of("*"));
-
+            List.of("*"));
         configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
+        UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
-    }
+}
 }
